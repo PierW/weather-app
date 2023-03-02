1e15aec25de219547e23898515df9b6557f52ac0
Pierpaolo Wurzburger <pierpaolo.wurzburger@meetweb.it>
2023-03-02 01:13:41 +0100

    inizio api con netlify serverless functions

--- src/components/CityList.vue
+++ src/components/CityList.vue
@@ -6,43 +6,59 @@
 
     const openWeatherAPIkey = import.meta.env.VITE_OPEN_WEATHER_API_KEY;
     const storageCities = ref([]);
+    const router = useRouter();
 
     const getCities = async () => {
-    if (localStorage.getItem("storageCities")) {
-        storageCities.value = JSON.parse(
-        localStorage.getItem("storageCities")
-        );
-        const requests = [];
-        storageCities.value.forEach((city) => {
-          requests.push(
-              axios.get(
-              `https://api.openweathermap.org/data/2.5/weather?lat=${city.coordinates.lat}&lon=${city.coordinates.lng}&appid=${openWeatherAPIkey}&units=metric`
-              )
+      if (localStorage.getItem("storageCities")) {
+          storageCities.value = JSON.parse(
+          localStorage.getItem("storageCities")
           );
-        });
+          const requests = [];
+          storageCities.value.forEach((city) => {
+            requests.push(
+                axios.get(
+                `https://api.openweathermap.org/data/2.5/weather?lat=${city.coordinates.lat}&lon=${city.coordinates.lng}&appid=${openWeatherAPIkey}&units=metric`
+                )
+            );
+          });
 
-        const weatherData = await Promise.all(requests);
-        // Flicker Delay
-        await new Promise((res) => setTimeout(res, 1000));
+          const weatherData = await Promise.all(requests);
+          // Flicker Delay
+          await new Promise((res) => setTimeout(res, 1000));
 
-        weatherData.forEach((value, index) => {
-        storageCities.value[index].weather = value.data;
-        });
-    }
+          weatherData.forEach((value, index) => {
+          storageCities.value[index].weather = value.data;
+          });
+      }
     };
     await getCities();
-    const router = useRouter();
+    
     const goToCityView = (city) => {
-    router.push({
-        name: "cityView",
-        params: { state: city.state, city: city.city },
-        query: {
-        lat: city.coordinates.lat,
-        lng: city.coordinates.lng,
-        id: city.id
-        },
-    });
+      router.push({
+          name: "cityView",
+          params: { state: city.state, city: city.city },
+          query: {
+          lat: city.coordinates.lat,
+          lng: city.coordinates.lng,
+          id: city.id
+          },
+      });
     };
+
+    const getNtlData = async (e) => {
+      e.preventDefault();
+      const res = await fetch('/.netlify/functions/openWeather');
+
+      if(!res.ok){
+        console.log(res);
+        return;
+      }
+
+      const data = await res.text();
+
+      document.querySelector('.output').textContent = data;
+
+    }
 </script>
 
 <template>
@@ -53,4 +69,6 @@
   <p v-if="storageCities.length === 0">
     Nessuna location salvata. Cercane una tramite l'input di ricerca sopra.
   </p>
+  <pre class="output"></pre>
+  <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" @click="getNtlData">Clicca</button>
 </template>
